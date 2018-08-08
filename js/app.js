74b75d2d952eaa5c725e2bf47517209d4c8c589c
Itang Samuel <31269938+shinnokmp@users.noreply.github.com>
2018-08-08 20:03:40 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ var Enemy = function(row, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -25,14 +25,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.speed * dt;
+    this.x += this.speed * dt;
     if (this.x > 500) this.x = -100;
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -45,17 +45,17 @@ var Player = function() {
 
     this.sprite = ['images/char-boy.png'];
 
-}
+};
 
 Player.prototype.update = function() {
     this.x = 200;
     this.y = 380;
     this.score = this.score + 1;
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.handleInput = function(key) { 
 
@@ -76,13 +76,13 @@ Player.prototype.handleInput = function(key) {
             this.y = this.y + 80;
         } 
     }
-}
+};
 
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 380;
     this.score = this.score - 1;    
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -94,13 +94,13 @@ for (var i = 0; i < 7; i++) {
     var random_speed = getRandomNumber(10, 31) * 10;
     var random_row = getRandomNumber(1, 4);
     allEnemies[i] = new Enemy(random_row, random_speed);
-}
+};
 
 var player = new Player();
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -142,7 +142,7 @@ function startTimer(){
             document.location.reload(true);  
         }
     },1000);
-}
+};
 
 //Displaying Player name on the page
 let playingName = document.querySelector("#name");
